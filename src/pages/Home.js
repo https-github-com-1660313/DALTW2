9632c575708bf9b4707e5aa3485d5144579c477f
1660313 <45199616+1660313@users.noreply.github.com>
2021-08-28 05:46:49 +0700

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,23 @@
+import React, { Fragment } from "react";
+import Carousel from "../components/Carousel/Carousel";
+import ListMovie from "../components/ListMovie/ListMovie";
+import News from "../components/News/News";
+import AppMobile from "../components/AppMobile/AppMobile";
+import ShowTimeHome from "../components/ShowTimeHome/ShowTimeHome";
+import ScrollAnimation from "react-animate-on-scroll";
+
+export default function Home() {
+  return (
+    <Fragment>
+      <Carousel />
+      <ListMovie />
+      <ScrollAnimation animateIn="fadeIn">
+        <ShowTimeHome />
+      </ScrollAnimation>
+      <ScrollAnimation animateIn="zoomIn">
+        <News />
+      </ScrollAnimation>
+      <AppMobile />
+    </Fragment>
+  );
+}
